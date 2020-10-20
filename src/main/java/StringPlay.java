2d49c681d2a874ff1bd87fb0f7cc8a0e6cfc9fb7
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2020-10-20 13:49:55 -0500

    feat: utilize StringUtils.isNumeric() to print out a line of text if the input is numeric and if an input is not numeric

--- src/main/java/StringPlay.java
+++ src/main/java/StringPlay.java
@@ -1,10 +1,18 @@
 import java.util.Scanner;
+import org.apache.commons.lang3.StringUtils;
 
 public class StringPlay {
 
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string: ");
     String input = scanner.nextLine();
-        System.out.println(input);
+        System.out.println("You Entered: \"" + input + "\"");
+        if (StringUtils.isNumeric(input)) {
+            System.out.println("\""+input+"\" is a number");
+        } else {
+            System.out.println("\""+input+"\" is not a number");
+        }
+
     }
 }
